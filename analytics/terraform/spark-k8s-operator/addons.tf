0a91444a17deebef3249f16f3040408fc1250177
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2023-03-02 15:42:28 -0800

    Update addons.tf
    Updated source link to main EKS blueprint repo:  "https://github.com/aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform//modules/kubernetes-addons"

--- analytics/terraform/spark-k8s-operator/addons.tf
+++ analytics/terraform/spark-k8s-operator/addons.tf
@@ -1,8 +1,8 @@
 module "eks_blueprints_kubernetes_addons" {
   #source = "github.com/aws-ia/terraform-aws-eks-blueprints//modules/kubernetes-addons?ref=v4.19.0"
-  #DZ: need to change reference to the main EKS Blueprint repo to github.com/aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform
-  #need to be double checked
-  source = "git::https://aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform.git//modules/kubernetes-addons" 
+  #need to change reference to the main EKS Blueprint repo to github.com/aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform
+  #TODO: validate once main EKS repo is public
+  source = "https://github.com/aws-solutions-library-samples/guidance-for-automated-provisioning-of-amazon-elastic-kubernetes-service-using-terraform//modules/kubernetes-addons" 
   eks_cluster_id       = module.eks_blueprints.eks_cluster_id
   eks_cluster_endpoint = module.eks_blueprints.eks_cluster_endpoint
   eks_oidc_provider    = module.eks_blueprints.oidc_provider
